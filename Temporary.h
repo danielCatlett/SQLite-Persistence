c6ae1d0c5f87107793b350a00f07374ce9194187
danielCatlett <daniel.catlett42@gmail.com>
2017-05-10 01:32:15 -0500

    SQLite Persistence

---
+++ Temporary.h
@@ -0,0 +1,13 @@
+//
+//  Temporary.h
+//  SQLite Persistence
+//
+//  Created by Daniel Catlett on 5/8/17.
+//  Copyright © 2017 Daniel Catlett. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Temporary : NSObject
+
+@end
---
+++ Temporary.m
@@ -0,0 +1,11 @@
+//  Temporary.m
+//  SQLite Persistence
+//
+//  Created by Daniel Catlett on 5/8/17.
+//  Copyright © 2017 Daniel Catlett. All rights reserved.
+
+#import "Temporary.h"
+
+@implementation Temporary
+
+@end
